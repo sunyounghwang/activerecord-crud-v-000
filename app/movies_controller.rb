7652584dd433a17ced60b5a2db1c12dc8feb06b8
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-20 02:25:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,7 +63,6 @@ def can_be_found_updated_and_saved
   binding.pry
   movie.title = "Even Awesomer Flick"
   movie.save
-  Movie.find_by(title: "Even Awesomer Flick")
 end
 
 def can_update_using_update_method
